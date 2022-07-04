621c9f7c342ad42ffb7edea2d4c10deaf2a0e951
IbrahimElmourchidi <ibrahimelmourchidi@gmail.com>
2022-07-04 22:37:32 +0200

    kdjfkd

--- client/webclient-bootstrap/src/environments/environment.prod.ts
+++ client/webclient-bootstrap/src/environments/environment.prod.ts
@@ -2,7 +2,7 @@ export const environment = {
   production: true,
   // authRoot: 'https://learning-lantern-auth-api.herokuapp.com/user',
   authRoot: 'https://learning-lantern.azurewebsites.net/api/user/',
-  chatRoot: 'https://learning-lantern-chat-api.herokuapp.com',
+  chatRoot: 'https://learning-lantern-chat-api.herokuapp.com/',
   calendarRoot: 'https://learning-lantern.azurewebsites.net/api/Calendar',
   todoRoot: 'https://learning-lantern.azurewebsites.net/api/v1/Tasks',
 };
